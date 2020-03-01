53d326ad80c374d4272fed9a38546eff08d132e9
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-03-01 06:02:27 -0400

    fix

--- coeficienteDePearson.py
+++ coeficienteDePearson.py
@@ -1,13 +1,19 @@
 def coeficiente_de_correlacao(dado1, dado2):
     
-    mediaX = 0.0
-    mediaY = 0.0
+    somaX = 0.0
+    somaY = 0.0
+    somaXY = 0.0
+    somaXQuad = 0.0
+    somaYQuad = 0.0
     for i in range(len(dado1)):
         
-        mediaX += float(dado1[i] / len(dado1))
-        mediaY += float(dado2[i] / len(dado2))
-    resultado = 0.0
-    for i in range(len(dado1)):
-        resultado += (dado1[i] - mediaX) * (dado2[i] - mediaY)
-        resultado /= ((dado1[i] - mediaX)**2 * (dado2[i] - mediaY)**2) ** (1/2)
+        somaX += float(dado1[i])
+        somaY += float(dado2[i])
+        somaXY += float(dado1[i]) * float(dado2[i])
+        somaXQuad += float(dado1[i]) ** 2
+        somaYQuad += float(dado2[i]) ** 2
+
+    #aplicando fórmula do Coeficiente de Correlação de Postos de Pearson
+    resultado = somaXY - (somaX * somaY / (len(dado1)))
+    resultado /= ((somaXQuad - ((somaX ** 2.0) / len(dado1))) * (somaYQuad - ((somaY ** 2.0) / len(dado1)))) ** (1/2)
     return resultado
